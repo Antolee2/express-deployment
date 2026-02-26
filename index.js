55dc29cd363ea1556c62007616f9b279ed88a151
Antonlee <leeroychivandire2022@gmail.com>
2026-02-26 12:38:28 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const express=require('express');
+const path=require('path');
+const pug=require('pug');
+const app=express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.get('/all',(req, res)=>{
+    const subjects=['DevOps','Cloud Computing','SPM','RES','CNS',];
+    res.render('sample.pug', {sub:subjects});
+});
+module.exports=app;
