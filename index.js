c92af34ad146a8a7a12791c8b3a09901f24ab21a
Antonlee <leeroychivandire2022@gmail.com>
2026-02-27 12:03:26 +0530

    second

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.get('/all',(req, res)=>{
-    const subjects=['DevOps','Cloud Computing','SPM','RES','CNS',];
+    const subjects=['DevOps','Cloud Computing','SPM','RES','CNS','Garo'];
     res.render('sample.pug', {sub:subjects});
 });
 module.exports=app;
